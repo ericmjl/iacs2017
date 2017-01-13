5324ee88159427eba3a63691f86f2b715205b35e
Eric J. Ma <ericmajinglong@gmail.com>
2017-01-13 17:46:21 -0500

    minor updates to bokehscatter

--- bokehscatter.py
+++ bokehscatter.py
@@ -43,50 +43,48 @@ quantcols = ['Elevation', 'Aspect', 'Slope',
 
 
 def create_scatter1():
-    x_title = x.value.title()
-    y1_title = y1.value.title()
-    print(x_title, y1_title)
+    x_title = x1.value.title()
+    y_title = y1.value.title()
+    print(x_title, y_title)
 
     kw = dict()
-    kw['title'] = "{0} vs {1}".format(x_title, y1_title)
+    kw['title'] = "{0} vs {1}".format(x_title, y_title)
 
     f1 = figure(plot_height=400, plot_width=400, tools=TOOLS, **kw)
-    f1.scatter(x=x_title, y=y1_title, color='colors', source=src)
+    f1.scatter(x=x_title, y=y_title, color='colors', source=src)
     f1.xaxis.axis_label = x_title
-    f1.yaxis.axis_label = y1_title
+    f1.yaxis.axis_label = y_title
     return f1
 
 
 def create_scatter2():
-    x_title = x.value.title()
-    y2_title = y2.value.title()
-    print(x_title, y2_title)
+    x_title = x2.value.title()
+    y_title = y2.value.title()
+    print(x_title, y_title)
 
     kw = dict()
-    kw['title'] = "{0} vs {1}".format(x_title, y2_title)
+    kw['title'] = "{0} vs {1}".format(x_title, y_title)
 
     f2 = figure(plot_height=400, plot_width=400, tools=TOOLS, **kw)
-    f2.scatter(x=x_title, y=y2_title, color='colors', source=src)
+    f2.scatter(x=x_title, y=y_title, color='colors', source=src)
     f2.xaxis.axis_label = x_title
-    f2.yaxis.axis_label = y2_title
+    f2.yaxis.axis_label = y_title
     return f2
 
 
-def update(attr, old, new):
-    update1(attr, old, new)
-    update2(attr, old, new)
-
-
 def update1(attr, old, new):
-    layout.children[1] = create_scatter1()
+    r1.children[1] = create_scatter1()
 
 
 def update2(attr, old, new):
-    layout.children[2] = create_scatter2()
+    r1.children[2] = create_scatter2()
 
 
-x = Select(title='X-Axis', value='Elevation', options=quantcols)
-x.on_change('value', update)
+x1 = Select(title='X1-Axis', value='Elevation', options=quantcols)
+x1.on_change('value', update1)
+
+x2 = Select(title='X2-Axis', value='Elevation', options=quantcols)
+x2.on_change('value', update2)
 
 y1 = Select(title='Y1-Axis', value='Horizontal_Distance_To_Hydrology',
             options=quantcols)
@@ -96,8 +94,8 @@ y2 = Select(title='Y2-Axis', value='Aspect',
             options=quantcols)
 y2.on_change('value', update2)
 
-controls = widgetbox([x, y1, y2], width=300)
-layout = row(controls, create_scatter1(), create_scatter2())
+controls = widgetbox([x1, y1, x2, y2], width=300)
+r1 = row(controls, create_scatter1(), create_scatter2())
 
-curdoc().add_root(layout)
+curdoc().add_root(r1)
 curdoc().title = "Quantitative Variable Explorer"
